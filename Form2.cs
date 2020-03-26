15b0a9039237e36dcc5eb9db9bbdd5aa9e84600f
x4SVxx <sergey.chernyh.2001@mail.ru>
2020-03-26 16:06:15 +0300

    progressbar

---
+++ Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            // Делаем обычный стиль.
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            // Убираем кнопки свернуть, развернуть, закрыть.
+            this.ControlBox = false;
+            // Убираем заголовок.
+            this.Text = "Loading...";
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
